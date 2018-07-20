8341cddc89040b56c4c98d4c818e9831ac12d725
Adebanjo oluwatominiyin <tomiadebanjo@gmail.com>
2018-07-20 14:27:19 +0100

    feat(frontend): implement frontend ride requests
    [Delivers #159187459]

--- scripts/get-all-rides.js
+++ scripts/get-all-rides.js
@@ -3,8 +3,20 @@ const rideTable = document.querySelector('#ride-table');
 const modalTable = document.querySelector('#single-table');
 const rideSpinner = document.querySelector('#ride-spinner');
 const modalSpinner = document.querySelector('#modal-spinner');
-const modal = document.getElementById('myModal');
-const span = document.getElementById('ride-close');
+const modal = document.querySelector('#myModal');
+const span = document.querySelector('#ride-close');
+const sucMessage = document.querySelector('#ride-api-success');
+const errMessage = document.querySelector('#ride-api-error');
+
+// trim and reverse Date gotten from api function
+const trimDate = trim => trim
+  .slice(0, 10)
+  .split('-')
+  .reverse()
+  .join('-');
+
+// trim time gotten from api function
+const trimTime = trim => trim.slice(0, 5);
 
 rideSpinner.setAttribute('style', 'display: block');
 let rideInfo = '';
@@ -32,13 +44,18 @@ fetch('https://ride-my-way-zaz.herokuapp.com/api/v1/rides', {
                         <th>Details</th>
                     </tr >`;
           data.rides.forEach((ride) => {
+            console.log(ride);
+            const newDate = trimDate(ride.departure_date);
+            const newTime = trimTime(ride.departure_time);
             rideInfo += ` <tr class="row">
                             <td>${ride.pickup_location}</td>
                             <td>${ride.destination}</td>
-                            <td>${ride.departure_time}</td>
-                            <td>${ride.departure_date}</td>
+                            <td>${newTime}</td>
+                            <td>${newDate}</td>
                             <td>
-                                <button class="btn-light" onclick="rideDetails(${ride.id})">View ride details / Request ride</button>
+                                <button class="btn-light" onclick="rideDetails(${
+  ride.id
+})">View ride details / Request ride</button>
                             </td>
                     </tr>`;
             rideTable.innerHTML = rideInfo;
@@ -54,6 +71,8 @@ const rideDetails = (rideId) => {
   let singleRideInfo = '';
   modal.style.display = 'block';
   modalTable.innerHTML = '';
+  errMessage.setAttribute('style', 'display: none');
+  sucMessage.setAttribute('style', 'display: none');
   modalSpinner.setAttribute('style', 'display: block');
   span.onclick = () => {
     modal.style.display = 'none';
@@ -76,9 +95,15 @@ const rideDetails = (rideId) => {
       setTimeout(() => {
         modalSpinner.setAttribute('style', 'display: none');
         if (data.success) {
+          const trimmedDate = trimDate(data.rideDetails.departure_date);
+          const trimmedTime = trimTime(data.rideDetails.departure_time);
           console.log(data);
           singleRideInfo = `
             <table class="table">
+              <tr class="row">
+                <th>Driver name</th>
+                <td>${data.rideDetails.fullname}</td>
+              </tr>
               <tr class="row">
                 <th>Take-off Location</th>
                 <td>${data.rideDetails.pickup_location}</td>
@@ -89,17 +114,47 @@ const rideDetails = (rideId) => {
               </tr>
               <tr class="row">
                 <th>Departure Date</th>
-                <td>${data.rideDetails.departure_date}</td>
+                <td>${trimmedDate}</td>
               </tr>
               <tr class="row">
                 <th>Departure Time</th>
-                <td>${data.rideDetails.departure_time}</td>
+                <td>${trimmedTime}</td>
               </tr>
             </table>
-            <button class="modal-btn">REQUEST TO JOIN RIDE</button>
+            <div class="loader" id="ri-spinner"></div>
+            <button class="modal-btn" onclick="joinRide(${rideId})">REQUEST TO JOIN RIDE</button>
           `;
           modalTable.innerHTML = singleRideInfo;
         }
       }, 1000);
+    });
+};
+
+// Request to join ride
+const joinRide = (rideId) => {
+  const rideSpinner = document.querySelector('#ri-spinner');
+  rideSpinner.setAttribute('style', 'display: block');
+  const joinUrl = `https://ride-my-way-zaz.herokuapp.com/api/v1/rides/${rideId}/requests`;
+  fetch(joinUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: sessionStorage.token,
+    },
+  })
+    .then(res => res.json())
+    .then((data) => {
+      setTimeout(() => {
+        rideSpinner.setAttribute('style', 'display: none');
+        console.log(data);
+        if (data.success) {
+          sucMessage.setAttribute('style', 'display: block');
+          sucMessage.innerHTML = `<i class="fa fa-check"></i> ${data.message}`;
+        } else {
+          errMessage.setAttribute('style', 'display: block');
+          errMessage.innerHTML = `<i class="far fa-times-circle"></i> ${data.message}`;
+        }
+      }, 1000);
+      console.log(data);
     });
 };
---
+++ scripts/requests.js
@@ -0,0 +1,154 @@
+const reqError = document.querySelector('#view-ride-error');
+const reqTable = document.querySelector('#view-table');
+const reqModalTable = document.querySelector('#view-single-table');
+const reqSpinner = document.querySelector('#view-spinner');
+const reqModalSpinner = document.querySelector('#request-spinner');
+const reqModal = document.querySelector('#viewModal');
+const reqSpan = document.querySelector('#view-close');
+const reqSucMessage = document.querySelector('#view-api-success');
+const reqErrMessage = document.querySelector('#view-api-error');
+
+// trim and reverse Date gotten from api function
+const trimDate = trim => trim
+  .slice(0, 10)
+  .split('-')
+  .reverse()
+  .join('-');
+
+// trim time gotten from api function
+const trimTime = trim => trim.slice(0, 5);
+
+reqSpinner.setAttribute('style', 'display: block');
+let rideStatus = '';
+const usersId = Number(sessionStorage.userId);
+
+fetch('https://ride-my-way-zaz.herokuapp.com/api/v1/rides', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: sessionStorage.token,
+  },
+})
+  .then(res => res.json())
+  .then((data) => {
+    setTimeout(() => {
+      reqSpinner.setAttribute('style', 'display: none');
+      if (data.success) {
+        const requestData = data.rides.filter(ride => ride.userid === usersId);
+        console.log(requestData);
+        if (data.rides.length < 1) {
+          reqError.setAttribute('style', 'display: block');
+          reqError.innerHTML = '<i class="far fa-times-circle"></i> No available ride at this moment, check back later';
+        } else if (requestData.length < 1) {
+          reqError.setAttribute('style', 'display: block');
+          reqError.innerHTML = '<i class="far fa-times-circle"></i> You havent offered a ride yet !! Offer a ride to view ride status';
+        } else {
+          rideStatus = `<tr class="row">
+                        <th>Take-off location</th>
+                        <th>Destination</th>
+                        <th>Departure time</th>
+                        <th>Departure date</th>
+                        <th>Details</th>
+                    </tr >`;
+          requestData.forEach((ride) => {
+            const newDate = trimDate(ride.departure_date);
+            const newTime = trimTime(ride.departure_time);
+            rideStatus += ` <tr class="row">
+                            <td>${ride.pickup_location}</td>
+                            <td>${ride.destination}</td>
+                            <td>${newTime}</td>
+                            <td>${newDate}</td>
+                            <td>
+                                <button class="btn-light" onclick="getRideRequests(${ride.id})">
+View ride requests</button>
+                            </td>
+                    </tr>`;
+            reqTable.innerHTML = rideStatus;
+          });
+        }
+      }
+    }, 1000);
+  })
+  .catch(err => console.log(err));
+
+// Get details for a specific ride offer
+const getRideRequests = (rideId) => {
+  let singleRideInfo = '';
+  reqModal.style.display = 'block';
+  reqModalTable.innerHTML = '';
+  reqErrMessage.setAttribute('style', 'display: none');
+  reqSucMessage.setAttribute('style', 'display: none');
+  reqModalSpinner.setAttribute('style', 'display: block');
+  reqSpan.onclick = () => {
+    reqModal.style.display = 'none';
+  };
+  window.onclick = (event) => {
+    if (event.target == reqModal) {
+      reqModal.style.display = 'none';
+    }
+  };
+  const url = `https://ride-my-way-zaz.herokuapp.com/api/v1/users/rides/${rideId}/requests`;
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: sessionStorage.token,
+    },
+  })
+    .then(res => res.json())
+    .then((data) => {
+      console.log(data);
+      setTimeout(() => {
+        reqModalSpinner.setAttribute('style', 'display: none');
+        if (data.success) {
+          console.log(data);
+          singleRideInfo = `
+              <tr class="row">
+                <th>Rider name</th>
+                <th>Request status</th>
+                <th>Response</th>
+              </tr>`;
+          data.requests.forEach((request) => {
+            console.log(request);
+            const requestId = request.requestid;
+            const rideId = request.rideid;
+            singleRideInfo += `
+                <tr class="row">
+                    <td>${request.full_name}</td>
+                    <td>${request.request_status}</td>
+                    <td>
+                        <button class="btn-light btn-accept" onclick="requestResponse(${rideId},${requestId},'accepted')">Accept</button>
+                        <button class="btn-light btn-reject" onclick="requestResponse(${rideId},${requestId},'rejected')">Reject</button>
+                    </td>
+              </tr>`;
+          });
+          reqModalTable.innerHTML = singleRideInfo;
+        } else {
+          reqErrMessage.setAttribute('style', 'display: block');
+          reqErrMessage.innerHTML = '<i class="far fa-times-circle"></i> No requests found for this ride. Check back later';
+        }
+      }, 1000);
+    });
+};
+
+const requestResponse = (rideId, requestId, response) => {
+  const url = `https://ride-my-way-zaz.herokuapp.com/api/v1/users/rides/${rideId}/requests/${requestId}`;
+  const resReq = { response };
+  fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: sessionStorage.token,
+    },
+    body: JSON.stringify(resReq),
+  })
+    .then(res => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.success) {
+        setTimeout(()=> {
+            getRideRequests(rideId);
+        }, 1000)
+      }
+    });
+};
